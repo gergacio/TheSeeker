4691f306d0afc8bfd17533529fa1619cacc7df93
Georgi <ggeorgeuk@gmail.com>
2023-06-01 08:25:15 +0100

    add users models and routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,31 @@
 import express from "express"; //framework create our api
 import cors from "cors" // set up rules for comunications (between diff domains)
 import mongoose from "mongoose" // mongodb orm (alow us to write queries in easy way)
+import * as dotenv from 'dotenv';
+import { userRouter } from './routes/users.js'
+
 
 const app = express(); //genarate a virsion of our api
 
 //apply middleware usifulinside our app
 app.use(express.json()); //turn data in json
 app.use(cors()); //
+dotenv.config();
+//apply routers
+app.use("/auth", userRouter);
+
+//mongodb - Atlas (cloud service) - create db and deploy them
+
+mongoose.connect(`mongodb+srv://ggeorgeuk:${process.env.ATLAS_PASS}@the-seeker-db.j5w682j.mongodb.net/the-seeker-db?retryWrites=true&w=majority`,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+
+//server
 
 app.listen(3001, () => console.log("Server Started!"));
+
+
+
 
---
+++ server/src/models/Users.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required:true, unique: true},
+    password: {type: String, required: true},
+    // savedRecipes: [{type: mongoose.Schema.Types.ObjectId, ref: "teachers"}], //many to many
+});
+
+export const UserModel = mongoose.model("users", UserSchema);
+
+
+//database called the-seeker-db with collections users and teachers
+//model for our collection called users will be created base on schema provided
